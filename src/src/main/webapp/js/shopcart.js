f32b0b7e33cde03010bfc8dbcd11c4c71413442c
XeonWangYue <qiwang18@qq.com>
2020-07-07 19:31:51 +0800

    购物车框架完成

--- src/src/main/webapp/js/shopcart.js
+++ src/src/main/webapp/js/shopcart.js
@@ -1,54 +1,70 @@
 // JavaScript Document
 
 function onNumberChange(){
-	alert(document.getElementById("1").id);
+    var parent=arguments[0];
+    var price=parent.nextElementSibling;
+    var total=price.nextElementSibling;
+    console.log(arguments[1]);
+    console.log(Number(price.innerHTML));
+    total.innerHTML=Number(price.innerHTML)*arguments[1];
+    console.log(total);
 }
+function del(){
+    var tbody=document.getElementById("tbodyNode");
+    tbody.removeChild(document.getElementById("wudidakafei"));
+    //TODO 还要做数据库的相关操作
+}
+/**增加一行购物车数据
+ * @param foodId
+ * @param foodName
+ * @param food
+ */
 
-function addRow(){
+function addRow(foodId,foodName,foodPrice){
+//TODO 参数替换
 //获取表格
     var tbody=document.getElementById("tbodyNode");
+//查重
+    for(i=0;i<tbody.childNodes.length;i++){
+        if(tbody.childNodes[i].nodeType==1){
+            if(tbodyNode.childNodes[i].id==foodId)
+        }
+    }
 //增加一行
     var row=document.createElement("tr");
-    tbody.appendChild(row);
-//第一列
-    var col_product=document.createElement("td");
-    col_product.classList.add("row");
-//产品
-    var divp=document.createElement("div");
-    divp.classList.add("col-md-6");
-    divp.classList.add("themed-grid-col");
-    col_product.appendChild(divp);
-    //图片
+//图片
+    var col_pic=document.createElement("td");
     var pic=document.createElement("img");
-    pic.src="images/3.jpg";
-    pic.classList.add("img-cart");
     pic.alt="实物图";
-    divp.appendChild(pic);
-    //描述
-    var divp=document.createElement("div");
-    divp.classList.add("col-md-6");
-    divp.classList.add("themed-grid-col");
-    divp.classList.add("descripe");
-    divp.appendChild(document.createTextNode("超级无敌小咖啡"));
-    col_product.appendChild(divp);
-    row.appendChild(col_product);
-//价格
-    var col_product=document.createElement("td");
-    col_product.classList.add("table-content");
-    col_product.appendChild(document.createTextNode("25"));
-    row.appendChild(col_product);
+    pic.src="images/3.jpg";
+    col_pic.appendChild(pic);
+    row.appendChild(col_pic);
+//名称
+    var col_name=document.createElement("td");
+    col_name.appendChild(document.createTextNode("超级无敌小咖啡"))
+    row.appendChild(col_name);
 //数量
-    var col_product=document.createElement("td");
-    var divi=document.createElement("div");
+    var col_quantity=document.createElement("td");
     var inp=document.createElement("input");
     inp.type="number";
-    inp.classList.add("input-number");
+    inp.value="1";
+    inp.min="1";
+    inp.onChange="addRow()";
 
-    divi.appendChild(inp);
-    col_product.appendChild(divi);
-    row.appendChild(col_product);
+    col_quantity.appendChild(inp);
+    row.appendChild(col_quantity);
+//价格
+    var col_price=document.createElement("td");
+    col_price.appendChild(document.createTextNode("24"));
+    row.appendChild(col_price);
+//价格小计
+    var col_total=document.createElement("td");
+    col_total.appendChild(document.createTextNode("100"));
+    row.appendChild(col_total);
+//操作
+    var col_operation=document.createElement("td");
+    col_operation.appendChild(document.createTextNode("删除"));
+    row.appendChild(col_operation);
 
-    var col_product=document.createElement("td");
-    col_product.appendChild(document.createTextNode("Cell 1,4"));
-    row.appendChild(col_product);
+    tbody.appendChild(row);
 }
