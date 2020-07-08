589ea320008a6c12ecf63b5eec395b813913a5d0
XeonWangYue <qiwang18@qq.com>
2020-07-08 15:34:03 +0800

    后端小改

--- src/src/main/java/top/xeonwang/tmxk/controller/loginController.java
+++ src/src/main/java/top/xeonwang/tmxk/controller/loginController.java
@@ -1,13 +1,9 @@
 package top.xeonwang.tmxk.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/service")
 public class loginController {
-	@RequestMapping("/login")
-	public String loginCheck(HttpSe) {
-		
-	}
+
+
 }
