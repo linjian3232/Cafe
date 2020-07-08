5ca95c230a32b36474ebb4b605a8adcac2fa41f4
XeonWangYue <qiwang18@qq.com>
2020-07-08 13:41:50 +0800

    menu可筛选

--- src/src/main/webapp/js/menu.js
+++ src/src/main/webapp/js/menu.js
@@ -9,13 +9,16 @@ function changeSelect(checked,type){
 }
 
 function searchName(value){
-    var root=document.getElementByClassName("no-gutters");
-    for(int i=0;i<root.length;i++){
-        if(root.h6.indexOf(value)!=-1){
-            root.hidden=false;
+    var root=document.getElementsByClassName("no-gutters");
+    console.log(root.length);
+
+    for(i=0;i<root.length;i++){
+        var str=String(root[i].childNodes[1].innerHTML);
+        if( str.indexOf(value)!=-1){
+            root[i].parentNode.hidden=false;
         }
         else{
-            root.hidden=true;
+            root[i].parentNode.hidden=true;
         }
     }
 }
