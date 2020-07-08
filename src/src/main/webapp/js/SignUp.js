cc0c0d19d726662dca98a2074b659b92fe40e23b
linjian3232 <1134747717@qq.com>
2020-07-08 18:00:25 +0800

    添加登录注册页接口，上传参数列表

--- src/src/main/webapp/js/SignUp.js
+++ src/src/main/webapp/js/SignUp.js
@@ -94,7 +94,7 @@ function validate_form(){
 
     var emailReg=/^\w+([-+.]\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*$/;
 
-    var passwordReg=/^(?![0-9]+$)(?![a-zA-Z]+$)[0-9A-Za-z]{6}$/;
+    var passwordReg=/^(?![0-9]+$)(?![a-zA-Z]+$)[0-9A-Za-z]{6,16}$/;
 
     if(username != "" && emailReg.test(username)){
 
@@ -142,4 +142,41 @@ function hideURLbar() {
 
     window.scrollTo(0, 1);
 
+}
+function register() {
+    var userName = document.getElementById("username").value;
+    var password = document.getElementById("password").value;
+	var name=document.getElementById("name").value;
+	var birthday=document.getElementById("birthday").value;
+	var tel=document.getElementById("tel").value;
+	
+
+    if(userName==""){
+        alert("请输入用户名！");
+        return;
+    }
+	if(password="")
+		{
+			 alert("请输入密码！");
+        return;
+		}
+
+var obj={
+        "userName":userName,//这里面定义的变量名要跟数据库里面的字段相同
+        "password":password,
+	    "name":name,
+	    "birthday":birthday,
+	    "tel":tel
+
+    }
+    $.ajax({
+        url:"http://"+window.location.host+"register",//这里是你接口的路径，记住http://不要忘记,localhost是你本地的地址，如果要请求别人的请将别人的ip地址替换，后面的addInfoPerson是接口名字。
+        type:"POST",//请求方式是get还是post,我这里用的是post
+        contentType:"application/json",
+        data:JSON.stringify(obj),//传递的参数
+        dataType:"json",
+        success:function(){ //成功返回的回调函数
+            alert("登录成功");
+        }
+     });
 }
