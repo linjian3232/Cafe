1987f0f1116d279e9fe9c9e4cdbc0bff83540b70
Yhw109 <1095253764@qq.com>
2020-07-21 20:28:56 +0800

    修改了订单查询

--- src/src/main/webapp/js/search.js
+++ src/src/main/webapp/js/search.js
@@ -19,14 +19,16 @@ data = [
 function test()
 		{
 			var ht = '';
+			ht = ht+'<tr><th>订单编号</th><th>生成日期</th><th>订单状态</th></tr>';
   			for(var i=0;i<data.length;i++){//循环json对象，拼接tr,td的html
+				
 				ht = ht+'<tr>';
 				ht = ht + '<th>' + data[i].orderid + '</th>';
 				ht = ht + '<th>' + data[i].ordertime + '</th>';
 				ht = ht + '<th>' + data[i].orderstatus + '</th>';
 				ht = ht+'</tr>';
 			}
-  $('#order').append(ht);
+  $('#order').html(ht);
 		}
 
 function search(){
@@ -41,15 +43,17 @@ function search(){
 		function(data)
 		{
 			var ht = '';
+			ht = ht+'<tr><th>订单编号</th><th>生成日期</th><th>订单状态</th></tr>';
   			for(var i=0;i<data.length;i++){//循环json对象，拼接tr,td的html
+				
 				ht = ht+'<tr>';
 				ht = ht + '<th>' + data[i].orderid + '</th>';
 				ht = ht + '<th>' + data[i].ordertime + '</th>';
 				ht = ht + '<th>' + data[i].orderstatus + '</th>';
 				ht = ht+'</tr>';
 			}
-  $('#order').append(ht);
-		}
+  $('#order').html(ht);
+			}
 	}
 	)
 }
